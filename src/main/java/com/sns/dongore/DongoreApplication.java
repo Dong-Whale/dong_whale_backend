2772afd255503be64a3642a5bfbec88013016aa6
노현욱 <rohsikdak@khu.ac.kr>
2022-08-02 13:25:21 +0900

    Add : initializing project

---
+++ src/main/java/com/sns/dongore/DongoreApplication.java
@@ -0,0 +1,13 @@
+package com.sns.dongore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sns/dongore/DongoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns.dongore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
